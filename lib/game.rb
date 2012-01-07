cad3b39e957efb77f6eab2c16bb4c9063bc4ace0
Alexander Shestakov <Maksar.mail@gmail.com>
2012-01-07 16:57:20 +0300

    Writing main game logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,34 @@
 class Game
-  def self.create_new_game
-    Game.new
+  attr_reader :players
+  attr_reader :anagram
+
+  def initialize(anagram)
+    @players = {}
+    @anagram = anagram
+    @histogram = create_histogram anagram
+  end
+
+  def add_player player
+    @players[player.name] = player
+  end
+
+  def write_word player, word
+    player.add_word(word) if valid_word? word
+  end
+
+  private
+
+  def valid_word? word
+    create_histogram(word).
+        each_with_object(create_histogram(anagram)) {|(k,v), h| h[k] = (h[k] || 0) - v}.
+        values.
+        all? { |v| v >= 0 }
+  end
+
+  def create_histogram word
+    word.
+        chars.
+        group_by { |char| char }.
+        each_with_object({}) { |(k, v), h| h[k] = v.count }
   end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,10 +1,31 @@
 require 'spec_helper'
-
 require 'game'
 
 describe Game do
-  it 'creates new game' do
-    Game.create_new_game.should be_instance_of Game
-  end
+  context 'with created game' do
+    let(:word) { 'sample' }
+    let(:game) { Game.new(word) }
 
+    it 'adds only new players' do
+      game.add_player(stub(:name => 'Bob'))
+      game.add_player(stub(:name => 'Bob'))
+
+      game.players.should have(1).item
+    end
+
+    context 'with one player' do
+      let(:player) { stub(:name => 'Alice') }
+      before { game.add_player(player) }
+
+      it 'accepts correct anagram' do
+        player.should_receive(:add_word)
+        game.write_word(player, 'sam')
+      end
+
+      it 'declines incorrect anagram' do
+        player.should_not_receive(:add_word)
+        game.write_word(player, 'wrong')
+      end
+    end
+  end
 end
